c2a82a1b35e994e3d67204f99fd332498f5c02b3
dben <sam.dben@gmail.com>
2024-04-29 23:48:14 +0000

    basic lists of object

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,20 +1,20 @@
 export default function getListStudents() {
   const obj1 = {
     id: 1,
-    firstName:'Guillaume',
-    location:'San Francisco'
+    firstName: 'Guillaume',
+    location: 'San Francisco'
   };
 
   const obj2 = {
     id: 2,
-    firstName:'Jame',
-    location:'Columbia'
+    firstName: 'Jame',
+    location: 'Columbia'
   };
 
   const obj3 = {
     id:5,
-    firstName:'Serena',
-    location:'San Francisco'
+    firstName: 'Serena',
+    location: 'San Francisco'
   };
   return [obj1, obj2, obj3];
 }

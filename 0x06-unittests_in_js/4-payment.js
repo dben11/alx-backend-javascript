c62bd80b10dfb7d276631aed1e55e30e198c48b1
dben <sam.dben@gmail.com>
2024-06-21 07:53:10 +0000

    update

---
+++ 0x06-unittests_in_js/4-payment.js
@@ -0,0 +1,9 @@
+// Create lost account
+const Utils = require('./utils');
+
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+  const sum = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${sum}`);
+}
+
+module.exports = sendPaymentRequestToApi;

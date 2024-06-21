2d0b3e948f8bd75032280fe9261ccc6ae56182e7
dben <sam.dben@gmail.com>
2024-06-21 08:10:42 +0000

    update

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,5 +1,5 @@
 const { expect } = require('chai');
-const calculateNumber = require('./1-calcul');
+const calculateNumber = require('./2-calcul_chai');
 
 describe('#calculateNumber() with type SUM', () => {
   it('add 5 and 10', () => {
